98f8bda7b4b55b24176eaf33695e87aae1aa20b8
Clet54 <124776725+Clet54@users.noreply.github.com>
2023-03-22 05:59:32 -0400

    Commit

--- src/Views/About.php
+++ src/Views/About.php
@@ -32,8 +32,8 @@
       <div class='row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg'>
         <div class='col-lg-7 p-3 p-lg-5 pt-lg-3'>
           <h3 class='display-6 fw-bold lh-1'>{$posts->get_title()}</h3>
-          <p class='lead'>Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, 
-          responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+          <p class='lead'>We provide authentic Nigerian meals freshly cooked by hand, by chefs with many years’ experience catering for major events across the UK. Our meals are delivered fresh-chilled in special insulated boxes that keeps the food chilled like a fridge during transit. This allows us to safely deliver your food to you anywhere in mainland UK – that’s all of England, Scotland and Wales</p>
+          
         </div>
         <div class='col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg'>
           <img class='rounded' src='{$posts::get_assets_url('/images/791.jpeg')}' alt='Jellof Rice' title='Nigeria Jellof Rice' width='720'>

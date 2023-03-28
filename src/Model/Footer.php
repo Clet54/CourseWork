b25f8ae8f19f59e5755e38bfe429940b7a09a815
Clet54 <124776725+Clet54@users.noreply.github.com>
2023-03-28 05:08:18 -0400

    Update Footer.php

--- src/Model/Footer.php
+++ src/Model/Footer.php
@@ -53,7 +53,7 @@
         <p class='col-md-4 mb-0 text-muted'>&COPY; 2023 Cletus Chigozie Opara</p>
 
         <a href='$home_url' class='col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none'>
-        <img class='bi me-2' src='{$posts->get_assets_url('images/favicons/f505307344.png')}' width='40' height='32'  alt='Logo'/>
+        <img class='bi me-2' src='{$posts->get_assets_url('images/favicons/2023.jpeg')}' width='40' height='32'  alt='Logo'/>
       </a>
 
         <ul class='nav col-md-4 justify-content-end text-capitalize'>
